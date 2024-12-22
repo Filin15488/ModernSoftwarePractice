dee31c6dcc4e93189b8f8eda5f661ada6b6b1891
Filin15488 <filya13579@yandex.ru>
2024-12-22 05:05:31 +0300

    write working main-window

--- src/main/java/com/Filin/Main.java
+++ src/main/java/com/Filin/Main.java
@@ -1,18 +1,46 @@
 package com.Filin;
 
+import com.Filin.View.Window;
 import com.Filin.View.WindowEmployee;
 import com.Filin.View.WindowRole;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends Window {
     private static WindowEmployee windowEmployee = new WindowEmployee();
     private static WindowRole windowRole = new WindowRole();
     public static void main(String[] args) {
-        windowEmployee.draw();
-        windowEmployee.setVisible(true);
+        new Main();
+    }
+    public Main() {
+        frame.setVisible(true);
+        frame.setTitle("Main Window");
+//        frame.setLayout();
+        frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        windowRole.draw();
-        windowRole.setVisible(true);
+        // Установка JMenuBar
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
 
+        JMenuItem employeesItem = new JMenuItem("Сотрудники");
+        JMenuItem rolesItem = new JMenuItem("Должности");
 
+        // Добавление обработчиков
+        employeesItem.addActionListener(e -> {
+            windowEmployee.refreshTable();
+            windowEmployee.setVisible(true);
+        });
+
+        rolesItem.addActionListener(e -> {
+            windowRole.refreshTable();
+            windowRole.setVisible(true);
+        });
+
+        menuBar.add(employeesItem);
+        menuBar.add(rolesItem);
+
+        frame.setJMenuBar(menuBar);
     }
 }
